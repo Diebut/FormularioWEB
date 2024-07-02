4fc940c4a45b87d2a2d8451ab5fdd2029323c3a3
Diego <guevaradiego_2011@hotmail.com>
2024-07-02 20:16:45 -0300

    se  modifica js validacion y agrega codigo en html

--- Js/registroCliente.js
+++ Js/registroCliente.js
@@ -1,5 +1,20 @@
 document.getElementById('registroForm').addEventListener('submit', function(event){
     event.preventDefault();
 
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+    const acceptTerms  = document.getElementById('acceptTerms').value;
+    const errorMessage  = document.getElementById('errorMessage ').value;
+
+    if (password !== confirmPassword) {
+        errorMessage.textContent = 'La Contraseña no coincide!';
+        errorMessage.style.display = 'Bloquear'
+    }else if(!acceptTerms){
+        errorMessage.textContent = 'Debe aceptar los términos y condiciones.'
+        errorMessage.style.display = 'Bloquear'
+    }else{
+        errorMessage.style.display = 'none';
+    }
+
     window.location.href = 'login.html';
 })
